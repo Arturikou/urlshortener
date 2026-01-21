dd2d0c570eba0c26e8b955f37a6d6384bee03b7f
arthurDerkach <artur.derkach@indriver.com>
2026-01-21 09:23:29 +0200

    feat: add env

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	hc "github.com/Arturikou/urlshortener/internal/handlers/config"
+	"os"
 )
 
 type Config struct {
@@ -15,6 +16,14 @@ func New() *Config {
 	flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "The address to listen on for HTTP requests.")
 	flag.StringVar(&cfg.Handlers.BaseAddr, "b", "http://localhost:8080", "The address for shortener response")
 	flag.Parse()
+
+	if envServerAddr := os.Getenv("SERVER_ADDRESS"); envServerAddr != "" {
+		cfg.ServerAddr = envServerAddr
+	}
+
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		cfg.Handlers.BaseAddr = envBaseAddr
+	}
 
 	return cfg
 }
